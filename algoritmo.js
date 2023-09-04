98c0610d0f455275c505fde4683dae37c5988628
noegunner <noesosa81@gmail.com>
2023-09-04 02:34:00 -0300

    primer commit

---
+++ algoritmo.js
@@ -0,0 +1,27 @@
+const divTablero = document.querySelector("#tablero");
+
+const MAX_FILA = 3;
+const MAX_COL = 3;
+
+for (let fila = 0; fila < MAX_FILA; fila++) {
+   /*  matriz.push([]) */
+   divTablero.innerHTML += `
+   <div class="row">
+   ${agregarColumnas()}
+
+   </div
+   `
+}
+  
+    
+    function agregarColumnas() {
+        let columnas = "";
+        for (let columna = 0; columna <MAX_COL; columna++) {
+            columnas += `
+            <div class="col casilla"> </div>
+            `;
+    }
+    return columnas;
+}
+    
+    
